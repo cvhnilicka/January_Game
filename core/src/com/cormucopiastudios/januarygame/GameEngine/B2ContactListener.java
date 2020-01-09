e36bfab54eb7a321ad99f141a9efe0972d573074
cvhnilicka <cvhnilicka@wisc.edu>
2020-01-08 20:18:54 -0800

    adding the beginnings of the contact listener

---
+++ core/src/com/cormucopiastudios/januarygame/GameEngine/B2ContactListener.java
@@ -0,0 +1,36 @@
+package com.cormucopiastudios.januarygame.GameEngine;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+public class B2ContactListener implements ContactListener {
+
+    private B2Model parent;
+
+    public B2ContactListener(B2Model parent) {
+        this.parent = parent;
+    }
+
+
+    @Override
+    public void beginContact(Contact contact) {
+
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
+    }
+}
