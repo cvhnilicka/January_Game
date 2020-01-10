431202c449960f57ac12401b1b0a53e048c26c5d
cvhnilicka <cvhnilicka@wisc.edu>
2020-01-09 17:15:29 -0800

    adding polygon bodies and cone sensor methods

--- core/src/com/cormucopiastudios/januarygame/GameEngine/Factories/BodyFactory.java
+++ core/src/com/cormucopiastudios/januarygame/GameEngine/Factories/BodyFactory.java
@@ -1,5 +1,6 @@
 package com.cormucopiastudios.januarygame.GameEngine.Factories;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
@@ -14,6 +15,9 @@ public class BodyFactory {
 
     private World world;
     private static BodyFactory thisInstance;
+
+
+    private final float DEGTORAD = 0.0174533f;
 
 
     private BodyFactory(World world) {
@@ -115,6 +119,56 @@ public class BodyFactory {
     public Body makeBoxPolyBody(float posx, float posy, float width, float height,FIXTURE_TYPE mat,
                                 BodyDef.BodyType bodyType){
         return makeBoxPolyBody(posx, posy, width, height, mat, bodyType, false);
+    }
+
+
+    /**
+     *
+     * Begin Poly Bodies
+     * */
+
+    /**
+     * This can make convex polygons
+     * */
+    public Body makePolygonShapeBody(Vector2[] vertices, float posx, float posy, FIXTURE_TYPE mat,
+                                     BodyDef.BodyType bodyType) {
+        BodyDef bdef = new BodyDef();
+        bdef.type = bodyType;
+        bdef.position.x = posx;
+        bdef.position.y = posy;
+        Body polyBody = world.createBody(bdef);
+
+        PolygonShape polygon = new PolygonShape();
+        polygon.set(vertices);
+        polyBody.createFixture(makeFixture(mat,polygon));
+        polygon.dispose();
+
+        return polyBody;
+    }
+
+    /**
+     * This can make cone shaped bodys. This is useful to use as a sensor to act as an enemies
+     * field of view.
+     * */
+    public void makeConeSensor(Body body, float size) {
+        FixtureDef fdef = new FixtureDef();
+//        fdef.isSensor = true; // will uncomment later
+
+        PolygonShape polygon = new PolygonShape();
+        float radius = size;
+        Vector2[] vertices = new Vector2[5];
+        vertices[0] = new Vector2(0,0);
+        for (int i = 2; i < 6; i++) {
+            float angle = (float)(i/6.0*145*DEGTORAD); // convert to radians
+            vertices[i-1] = new Vector2(radius*((float)Math.cos(angle)),
+                    radius*((float)Math.sin(angle)));
+        }
+
+        polygon.set(vertices);
+        fdef.shape = polygon;
+        body.createFixture(fdef);
+        polygon.dispose();
+
     }
 
 
