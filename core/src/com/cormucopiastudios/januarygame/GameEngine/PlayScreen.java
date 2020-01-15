7e17d95178545571668250668e4c0925fa7b765f
cvhnilicka <cvhnilicka@wisc.edu>
2020-01-14 17:30:56 -0800

    scrolling screeeeen beginning

--- core/src/com/cormucopiastudios/januarygame/GameEngine/PlayScreen.java
+++ core/src/com/cormucopiastudios/januarygame/GameEngine/PlayScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -29,7 +30,7 @@ public class PlayScreen implements Screen {
     private SpriteBatch batch;
 
 
-    private Sprite backgroundSprite;
+    private Sprite[] backgroundSprites;
 
     private Texture playerTex;
 
@@ -42,12 +43,46 @@ public class PlayScreen implements Screen {
         this.assMan = this.game.getParent().assMan;
         assMan.queueAddImages();
         assMan.manager.finishLoading();
-        backgroundSprite = new Sprite((Texture)assMan.manager.get(assMan.background));
-        backgroundSprite.setBounds(-gamecam.viewportWidth/2,-gamecam.viewportHeight/2,gamecam.viewportWidth,gamecam.viewportHeight);
-        backgroundSprite.setPosition(-gamecam.viewportWidth/2,-gamecam.viewportHeight/2);
+        backgroundSetUp();
         playerTex = assMan.manager.get(assMan.player, Texture.class);
         model = new B2Model(this);
         debugRenderer = new Box2DDebugRenderer(true,true,true,true,true,true);
+    }
+
+    private void backgroundSetUp() {
+        backgroundSprites = new Sprite[3];
+        backgroundSprites[0] = new Sprite((Texture)assMan.manager.get(assMan.background));
+        backgroundSprites[0].setBounds(-gamecam.viewportWidth/2,-gamecam.viewportHeight/2,gamecam.viewportWidth,gamecam.viewportHeight);
+        backgroundSprites[0].setPosition(-gamecam.viewportWidth/2,-gamecam.viewportHeight/2);
+
+        backgroundSprites[1] = new Sprite((Texture)assMan.manager.get(assMan.background));
+        backgroundSprites[1].setBounds(-gamecam.viewportWidth/2,-gamecam.viewportHeight/2,gamecam.viewportWidth,gamecam.viewportHeight);
+        backgroundSprites[1].setPosition(-gamecam.viewportWidth/2,backgroundSprites[0].getY()+gamecam.viewportHeight);
+
+        backgroundSprites[2] = new Sprite((Texture)assMan.manager.get(assMan.background));
+        backgroundSprites[2].setBounds(-gamecam.viewportWidth/2,-gamecam.viewportHeight/2,gamecam.viewportWidth,gamecam.viewportHeight);
+        backgroundSprites[2].setPosition(-gamecam.viewportWidth/2,backgroundSprites[1].getY()+gamecam.viewportHeight);
+    }
+
+    private void drawBackground(Batch batch){
+        Gdx.app.log("Background Sprite[2] : POS", String.valueOf(backgroundSprites[2].getY()));
+        if (backgroundSprites[0].getY() < -(3*gamecam.viewportHeight/2)) {
+            backgroundSprites[0].setPosition(backgroundSprites[0].getX(), backgroundSprites[2].getY()+gamecam.viewportHeight);
+            Gdx.app.log("Background Sprite[0] : Switch", String.valueOf(backgroundSprites[0].getY()));
+        }
+        if (backgroundSprites[1].getY() < -(3*gamecam.viewportHeight/2)) {
+            backgroundSprites[1].setPosition(backgroundSprites[1].getX(), backgroundSprites[0].getY()+gamecam.viewportHeight);
+            Gdx.app.log("Background Sprite[1] : Switch", String.valueOf(backgroundSprites[1].getY()));
+        }
+        if (backgroundSprites[2].getY() < -(3*gamecam.viewportHeight/2)) {
+            backgroundSprites[2].setPosition(backgroundSprites[2].getX(), backgroundSprites[1].getY()+gamecam.viewportHeight);
+            Gdx.app.log("Background Sprite[2] : Switch", String.valueOf(backgroundSprites[2].getY()));
+        }
+        for (int i = 0; i < backgroundSprites.length; i++) {
+            backgroundSprites[i].draw(batch);
+            backgroundSprites[i].setPosition(backgroundSprites[i].getX(), backgroundSprites[i].getY()-.33f);
+        }
+
     }
 
     public OrthographicCamera getGamecam() { return gamecam; }
@@ -70,13 +105,8 @@ public class PlayScreen implements Screen {
         Gdx.gl.glClearColor(0f,0f,0f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         debugRenderer.render(model.world, gamecam.combined);
-//        Gdx.app.log("Render", String.valueOf(backgroundSprite.getY()));
-//        if (backgroundSprite.getY() < -gamecam.viewportHeight)
-//            backgroundSprite.setPosition(-gamecam.viewportWidth/2,-gamecam.viewportHeight/2);
-//        else
-//            backgroundSprite.setPosition(backgroundSprite.getX(), backgroundSprite.getY()-1);
         batch.begin();
-        backgroundSprite.draw(batch);
+        drawBackground(batch);
         model.draw(batch);
         batch.end();
 
