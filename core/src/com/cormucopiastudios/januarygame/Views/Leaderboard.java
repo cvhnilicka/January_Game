1dbf77282125da6d2aa5750f6eb497e33244ee95
cvhnilicka <cvhnilicka@wisc.edu>
2020-01-18 21:13:11 -0800

    idk

--- core/src/com/cormucopiastudios/januarygame/Views/Leaderboard.java
+++ core/src/com/cormucopiastudios/januarygame/Views/Leaderboard.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
